3c60ead168ff397f8bfbd76655841fecd07d6fd7
Gabriel Ramos <whosramoss@gmail.com>
2025-02-06 16:51:29 -0300

    chore: add utils

---
+++ src/utils/utils-timeline.ts
@@ -0,0 +1,35 @@
+class UtilsTimeline {
+  public actions: any[] = [{ background: '#292929' }];
+
+  public currentIndex = 0;
+
+  public limit = 200;
+
+  public add(object: any) {
+    console.log('added', object);
+    this.actions.push(object);
+    this.currentIndex = this.actions.length - 1;
+    if (this.actions.length > this.limit) {
+      this.actions.shift();
+    }
+  }
+
+  public undo() {
+    console.log('undo',this);
+    if (this.currentIndex > 0) {
+      this.currentIndex--;
+      return this.actions[this.currentIndex];
+    }
+  }
+
+  public redo() {
+    console.log('redo',this);
+    if (this.currentIndex < this.actions.length - 1) {
+      this.currentIndex++;
+      return this.actions[this.currentIndex];
+    }
+  }
+}
+
+const timeline = new UtilsTimeline();
+export default timeline;
